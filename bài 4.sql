b6d277f0eb066305c504e54f7a20194a606a642d
synnd <127763785+synnd@users.noreply.github.com>
2026-04-22 07:23:06 +0700

    Update bài 4.sql
    Updated comments to explain SQL query optimization techniques.

--- bài 4.sql
+++ bài 4.sql
@@ -3,10 +3,17 @@
 -- dữ liệu suốt quá trình tạo nhóm, tốn CPU vì máy tính pk tính toán cả các dữ liệu rác chưa được lọc
 --  cách 2(dùng where) : lọc dữ liệu xong mới nhóm, loại bỏ được nhiều bản rác, 
 -- chỉ tập trung vào tính toán trên các dữ liệu đã được dọn sạch  
+-- vi dụ có 10tr dữ liệu
+-- Cách 1 : sẽ pk xử lí 10tr dòng, tính toán case when cho từng dòng, pk giữ nhiều bộ nhớ kkhi group by 
+--  và có nhiều dữ liệu không cần thiết 
+-- ==> Dẫn đến việc tốn tài nguyên, query chậm rõ rệt 
+-- Cách 2 : cpu chỉ pk xử lí dữ liệu khi đã lọc bớt rất nhiều bản ghi , bộ nhớ RAM pk lưu trữ ít dữ liệu hơn
+-- ==> Kết quả là tính toán nhanh hơn, Query scale tốt hơn khi dữ liệu lơn
+
 
 -- câu lệnh hoàn chỉnh
-
-select *from bookings
+select hotel_id
+from bookings
 where status = 'COMPLETED'
 group by hotel_id
 having avg(total_price)>3000000 and count(booking_id)>50;
